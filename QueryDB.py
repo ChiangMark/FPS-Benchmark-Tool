144b28a55e8b0a1f58a1a52df113cbc6346939b1
ChiangMark <king03168@gmail.com>
2024-01-22 16:26:24 +0800

    feat: add new function to count and query by cameraId and personId

--- QueryDB.py
+++ QueryDB.py
@@ -3,6 +3,7 @@ import pypyodbc as pyodbc
 import datetime
 import time
 import configparser
+import csv
 
 config = configparser.RawConfigParser() #fix that url include %
 config.read("config.ini")
@@ -42,29 +43,45 @@ class querydb():
             file.write("\n")
             file.write(end_time)
             
-    def get_camera_number(self):
-        self.command = str("SELECT COUNT (camera_id) FROM [faceme_security].[dbo].[camera_info]")
-        self.mycursor.execute(self.command)
-        self.camera_number = (self.mycursor.fetchall())
-            
     def get_face_count(self):
         self.camID2 =  str(self.camID[0][0])
         self.command = f"SELECT COUNT(*) FROM [faceme_security].[dbo].[record] WHERE [camera_id]={self.camID2} AND [log_time] BETWEEN '{self.ID1time[0][0].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}' AND '{self.ID1time[0][1].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}'"
         self.mycursor.execute(self.command)
-        self.finalfacerecord = self.mycursor.fetchall()
-        print("face records: ", self.finalfacerecord[0][0])
+        self.finalFaceRecord = self.mycursor.fetchall()
+        print("face records: ", self.finalFaceRecord[0][0])
         
     def get_body_count(self):
         self.camID2 =  str(self.camID[0][0])
         self.command = f"SELECT COUNT(*) FROM [faceme_security].[dbo].[body_record] WHERE [camera_id]={self.camID2} AND [log_time] BETWEEN '{self.ID1time[0][0].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}' AND '{self.ID1time[0][1].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}'"
         self.mycursor.execute(self.command)
-        self.finalbodyrecord = self.mycursor.fetchall()
-        print("body records: ", self.finalbodyrecord[0][0])
+        self.finalBodyRecord = self.mycursor.fetchall()
+        print("body records: ", self.finalBodyRecord[0][0])
+        
+    def queryPersonIdCount(self):
+        self.command = f"SELECT [camera_id], COUNT(person_id) as totalCount FROM [faceme_security].[dbo].[record] WHERE [log_time] between '{self.ID1time[0][0].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}' AND '{self.ID1time[0][1].strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}' GROUP BY [camera_id]"
+        self.mycursor.execute(self.command)
+        totalCount = self.mycursor.fetchall()
+        csv_filename = 'totalCount.csv'
+        self.write_to_csv(totalCount, csv_filename)
+        
+    def write_to_csv(self, data, filename):
+        with open (filename, 'w', newline='') as csvfile:
+            fieldnames = ['cameraId', 'totalCount']
+            
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            for row in data:
+                row_dict = {'cameraId': row[0], 'totalCount': row[1]}
+                writer.writerow(row_dict)
+        
+        
 
 if __name__ == '__main__':
     DB = querydb()
     DB.get_camID()
     DB.get_time()
-    DB.get_camera_number()
-    DB.get_face_count()
-    DB.get_body_count()
+    # DB.get_face_count()
+    # DB.get_body_count()
+    DB.queryPersonIdCount()
+    
